54115bf65c586a7d8c780337a3d83d62a6e85998
Ilya <priezhaev.08@gmail.com>
2025-06-26 13:30:17 +0300

    Setup swift3 backend and openstack provider

---
+++ backend.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_version = ">=1.12.1"
+    required_providers {
+        openstack = {
+            source = "terraform-provider-openstack/openstack"
+            version = ">=3.2.0"
+        }
+    }
+    backend "swift3" {
+        region = "ru-region-one"
+        access_key = "~/.aws/credentials"
+        secret_key = "~/.aws/credentials"
+        bucket = "tf-bucket"
+        key = "terraform/state"
+    }
+}
+
+provider "openstack" {
+  user_name   = "admin"
+  tenant_name = "admin"
+  auth_url    = "http://10.1.1.3:5000/v3"
+  region      = "RegionOne"
+}
