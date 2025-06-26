3681b2f0e8fab7ca943ef733522e6a70f78a1e2e
Ilya <priezhaev.08@gmail.com>
2025-06-26 16:43:42 +0300

    Import exists external network

--- network.tf
+++ network.tf
@@ -3,6 +3,13 @@ resource "openstack_networking_network_v2" "selfservice" {
   admin_state_up = "true"
 }
 
+resource "openstack_networking_network_v2" "extern01" {
+  name           = "extern01"
+  admin_state_up = "true"
+  shared = false
+  external = true
+}
+
 resource "openstack_networking_subnet_v2" "selfservice_subnet" {
   name = "selfservice_subnet"
   network_id = openstack_networking_network_v2.selfservice.id
@@ -15,4 +22,17 @@ resource "openstack_networking_subnet_v2" "selfservice_subnet" {
   enable_dhcp = true
   dns_nameservers = ["8.8.8.8"]
   gateway_ip = "192.168.1.1"
+}
+
+resource "openstack_networking_subnet_v2" "extern01_subnet" {
+  name = "extern01_subnet"
+  network_id = openstack_networking_network_v2.extern01.id
+  cidr       = "10.1.1.0/24"
+  allocation_pool {
+    start = "10.1.1.100"
+    end   = "10.1.1.200"
+  }
+  ip_version = 4
+  enable_dhcp = false
+  gateway_ip = "10.1.1.1"
 }
