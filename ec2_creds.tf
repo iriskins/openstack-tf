dcca4ac7cef33ebff6ce6ac803826151b2cbd8ad
Ilya <priezhaev.08@gmail.com>
2025-06-27 10:21:59 +0300

    import ec2 creds from openstack

---
+++ ec2_creds.tf
@@ -0,0 +1,5 @@
+resource "openstack_identity_ec2_credential_v3" "ec2_key1" {
+    project_id = "966caa5f6b664a318fd7ead6280b4d84"
+    region = "RegionOne"
+    user_id = "58b2729b96b1439ea7de8ee2a87aa0a3"
+}
