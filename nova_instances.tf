1deb130194cb66040f61d2118623136c36ae2853
Ilya <priezhaev.08@gmail.com>
2025-06-27 10:51:05 +0300

    import instance from openstack

---
+++ nova_instances.tf
@@ -0,0 +1,15 @@
+resource "openstack_compute_instance_v2" "tvm01_iriskin_edu" {
+  region = "RegionOne"
+  name            = "tvm01.iriskin.edu"
+  image_id        = openstack_images_image_v2.Fedora-42.image_id
+  flavor_id       = openstack_compute_flavor_v2.cherry.flavor_id
+  security_groups = [openstack_networking_secgroup_v2.all.name]
+  availability_zone = "nova"
+  network {
+    access_network = false
+    fixed_ip_v4 = "192.168.1.66"
+    name = openstack_networking_network_v2.selfservice.name
+  }
+  power_state = "active"
+  hypervisor_hostname = "cc01.iriskin.edu"
+}
